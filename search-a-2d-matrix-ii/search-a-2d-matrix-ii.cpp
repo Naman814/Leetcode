e31d8109ec2d3cf6df1fa03ef927ed6ef6d14a22
Naman814 <73079318+Naman814@users.noreply.github.com>
2021-09-26 15:11:05 +0530

    Time: 96 ms (86.42%), Space: 15 MB (21.36%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int low=0, high=matrix[0].size()-1;
-        for(int i=0;i<matrix.size();i++){
-            if(target>=matrix[i][low] && target<=matrix[i][high]){
-                for(auto x:matrix[i]){
-                    if(x==target){
-                        return true;
-                    }
-                }
-            }
+    bool searchMatrix(vector<vector<int>>& M, int target) {
+      bool flag=false;
+    
+    int n=M.size(), m=M[0].size();
+    int i=0,j=m-1;
+    
+    while(i<n && j>=0)
+    {
+        if(M[i][j]==target)
+        {
+            flag=true;
+            break;
         }
-        return false; 
+        
+        else if(M[i][j]<target)
+            i++;
+        else
+            j--;
+    }
+    
+    return flag;
     }
 };
+
+  // int low=0, high=matrix[0].size()-1;
+  //       for(int i=0;i<matrix.size();i++){
+  //           if(target>=matrix[i][low] && target<=matrix[i][high]){
+  //               for(auto x:matrix[i]){
+  //                   if(x==target){
+  //                       return true;
+  //                   }
+  //               }
+  //           }
+  //       }
+  //       return false; 
