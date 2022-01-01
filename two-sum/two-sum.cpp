62cbd1bb0f85daa66d22455ab67266e1f85f8032
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-01-01 23:21:40 +0530

    Time: 8 ms (94.99%), Space: 10.8 MB (45.09%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& v, int target) {
+        unordered_map <int,int> m;
+        vector<int> ans;
+        for(int i=0;i<v.size();i++)
+        {
+            if(m.find(target-v[i])!=m.end())
+            {
+                ans.push_back(i);
+                ans.push_back(m[target-v[i]]);    
+                break;
+            }
+            else{
+                m[v[i]]=i;
+            }
+        }
+        return ans;
+    }
+};
