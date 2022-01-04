8ca65d07b3cc787a8c2dea7b7e6e574117fa22dd
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-01-05 03:03:37 +0530

    Time: 542 ms (5.05%), Space: 117.9 MB (54.76%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* getmiddle(ListNode* p){
+        if(!p) return NULL;
+        ListNode* s=new ListNode(0), *f=new ListNode(0);
+        s->next=p;
+        f->next=p;
+        while(f and f->next){
+            s=s->next;
+            f=f->next->next;
+        }
+        return s;
+    }
+    
+    ListNode* reverseList(ListNode* p){
+        if(!p) return NULL;
+        ListNode* prev=NULL, *curr=p, *next=NULL;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* mid=NULL;
+        mid=getmiddle(head);
+        mid->next=reverseList(mid->next);
+        ListNode *start=head , *q=mid->next;
+        while(q and start->val==q->val){
+            start=start->next;
+            q=q->next;
+        }
+        
+        mid->next=reverseList(mid->next);
+        if(q==NULL){
+            return 1;
+        }
+        return 0;
+        
+    }
+};
