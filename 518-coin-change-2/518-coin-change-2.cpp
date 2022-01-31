ecd421bed4965c92a0fe800e23f566e91cfe966a
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-01-31 23:59:20 +0530

    Time: 8 ms (93.06%), Space: 7 MB (89.81%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int> dp(amount+1);
+        dp[0] = 1;
+        for(int i=0;i<n;i++){
+            for(int j=coins[i];j<dp.size();j++){
+                dp[j] += dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
