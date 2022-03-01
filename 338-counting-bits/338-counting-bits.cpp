874aa0ff3d5ffa8b5e710bd014e4a235177ffa87
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-03-01 19:18:06 +0530

    Time: 7 ms (62.32%), Space: 7.7 MB (89.06%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            dp[i] = dp[i&(i-1)]+1;
+        }
+        return dp;
+    }
+};
