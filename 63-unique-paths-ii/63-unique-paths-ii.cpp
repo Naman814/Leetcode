329c962a68ee1fff81b5d75a050df7ad0150309b
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-05-20 12:39:15 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (41.40%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&grid,int i,int j,vector<vector<int>>&vis,vector<vector<int>>&mem){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] || vis[i][j]){
+    int solve(vector<vector<int>>&grid,int i,int j,vector<vector<int>>&mem){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]){
             return 0;
         }
         if(i==grid.size()-1 && j==grid[0].size()-1){
@@ -10,18 +10,14 @@ public:
         if(mem[i][j]!=-1){
             return mem[i][j];
         }
-    
-        vis[i][j] = 1;
-        int r = solve(grid,i,j+1,vis,mem);
-        int d = solve(grid,i+1,j,vis,mem);
-        vis[i][j]=  0;
+        int r = solve(grid,i,j+1,mem);
+        int d = solve(grid,i+1,j,mem);
         return mem[i][j] = r + d;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
         vector<vector<int>> mem(n,vector<int>(m,-1));
-        return solve(grid,0,0,vis,mem);
+        return solve(grid,0,0,mem);
     }
 };
