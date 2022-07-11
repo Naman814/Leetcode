e652d5c60e872adf83028a2cddb8e253c9195839
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-07-11 14:18:17 +0530

    Time: 35 ms (28.34%), Space: 13.3 MB (36.86%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    
-    bool solve(string &s,vector<string>& wordDict,unordered_map<string,bool>&mp,int idx,vector<int>&dp){
-        if(idx>=s.size()) return true;
-        if(dp[idx]!=-1) return dp[idx];
-        for(int i=idx;i<s.size();i++){
-            if(mp[s.substr(idx,i-idx+1)] && solve(s,wordDict,mp,i+1,dp)){
-                return dp[idx]= true;
-            }
 
+    bool solve(string &s,vector<int>&dp,unordered_set<string>&st,vector<string>&wordDict,int idx){
+        if(idx>=s.length()) return true;
+        if(dp[idx]!=-1) return dp[idx];
+        for(int i=idx;i<s.length();i++){
+            if(st.count(s.substr(idx,i-idx+1)) && solve(s,dp,st,wordDict,i+1)){
+                return dp[idx] = true;
+            }
         }
-        return dp[idx]= false;
+        return dp[idx] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string,bool> mp;
-        vector<int> dp(s.size()+2,-1);
-        for(auto x:wordDict){
-            mp[x] = true;
-        }
-        return solve(s,wordDict,mp,0,dp);
+        vector<int> dp(s.length()+1,-1);
+        unordered_set<string> st(wordDict.begin(),wordDict.end());
+        return solve(s,dp,st,wordDict,0);
     }
 };
