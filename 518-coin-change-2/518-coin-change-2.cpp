1b3172f1c7fac36ccdddc968067ab8d552eb1afb
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-07-21 00:30:24 +0530

    Time: 619 ms (5.00%), Space: 73.3 MB (5.08%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dp[3001][5001];
     int solve(int amt,vector<int>&coins,int idx){
-        if(idx==coins.size()){
+        if(idx<0){
             if(amt==0){
                 return 1;
             }
@@ -10,16 +10,16 @@ public:
         }
         if(dp[idx][amt]!=-1) return dp[idx][amt];
         if(coins[idx]>amt){
-            return dp[idx][amt] = solve(amt,coins,idx+1);
+            return dp[idx][amt] = solve(amt,coins,idx-1);
         }
-        else return dp[idx][amt] = solve(amt-coins[idx],coins,idx)+ solve(amt,coins,idx+1);
+        else return dp[idx][amt] = solve(amt-coins[idx],coins,idx)+ solve(amt,coins,idx-1);
         
        
 
     }
     int change(int amount, vector<int>& coins) {
         memset(dp,-1,sizeof(dp));
-        return solve(amount,coins,0);
+        return solve(amount,coins,coins.size()-1);
         
     }
 };
