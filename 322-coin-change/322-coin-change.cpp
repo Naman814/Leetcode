078dee80c7cf991bf38bf22f040b8055536fdf11
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-07-21 00:38:52 +0530

    Time: 103 ms (72.47%), Space: 11.5 MB (75.31%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dp[13][10001];
     int solve(vector<int>&coins,int amt,int idx){
-        if(idx==coins.size() || amt<=0){
+        if(idx<0 || amt<=0){
             if(amt==0){
                 return 0;
             }
@@ -10,15 +10,15 @@ public:
         }
         if(dp[idx][amt]!=-1) return dp[idx][amt];
         if(coins[idx]>amt){
-           return dp[idx][amt] = solve(coins,amt,idx+1)+0;
+           return dp[idx][amt] = solve(coins,amt,idx-1)+0;
         }
         else{
-            return  dp[idx][amt] = min(1+solve(coins,amt-coins[idx],idx) ,solve(coins,amt,idx+1));
+            return  dp[idx][amt] = min(1+solve(coins,amt-coins[idx],idx) ,solve(coins,amt,idx-1));
         }
     }
     int coinChange(vector<int>& coins, int amount) {
         memset(dp,-1,sizeof(dp));
-        int x = solve(coins,amount,0);
+        int x = solve(coins,amount,coins.size()-1);
         return x==INT_MAX-1 ? -1 : x;
     }
 };
