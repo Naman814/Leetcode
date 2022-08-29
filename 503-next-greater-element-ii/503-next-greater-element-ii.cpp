34a5c16c3e9b8413a49102a0ffa31157f5a43e2a
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-08-29 23:27:26 +0530

    Time: 85 ms (15.96%), Space: 24 MB (63.15%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -5,8 +5,8 @@ public:
         stack<int> s;
         vector<int> res(n,-1);
         for(int i=0;i<2*n;i++){
-            while(!s.empty() && nums[s.top()%n] < nums[i%n]){
-                res[s.top()%n] = nums[i%n];
+            while(!s.empty() && nums[s.top()] < nums[i%n]){
+                res[s.top()] = nums[i%n];
                 s.pop();
             }
             s.push(i%n);
