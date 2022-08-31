3fc99c6d1a061e9f388df45eb80b32ce181638e4
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-08-31 19:10:56 +0530

    Time: 915 ms (5.02%), Space: 85.3 MB (27.97%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,25 +1,39 @@
-  class Solution {
-    public:
-        int largestRectangleArea(vector<int> &height) {
-            
-            int ret = 0;
-            height.push_back(0);
-            vector<int> index;
-            
-            for(int i = 0; i < height.size(); i++)
-            {
-                while(index.size() > 0 && height[index.back()] >= height[i])
-                {
-                    int h = height[index.back()];
-                    index.pop_back();
-                    
-                    int sidx = index.size() > 0 ? index.back() : -1;
-                    if(h * (i-sidx-1) > ret)
-                        ret = h * (i-sidx-1);
-                }
-                index.push_back(i);
+class Solution {
+public:
+    void nextSmaller(vector<int>&heights,vector<int>&left){
+        stack<int> s;
+        for(int i=0;i<heights.size();i++){
+            while(!s.empty() && heights[s.top()]>heights[i]){
+                left[s.top()] = i;
+                s.pop();
             }
-            
-            return ret;
+            s.push(i);
         }
-    };
+    }
+    void previousSmaller(vector<int>&heights,vector<int>&right){
+        stack<int> s;
+        for(int i=heights.size()-1;i>=0;i--){
+            while(!s.empty() && heights[s.top()]>heights[i]){
+                right[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n,n);
+        vector<int> right(n,-1);
+        nextSmaller(heights,left);
+        previousSmaller(heights,right);
+        for(int i=0;i<n;i++){
+            cout<<right[i]<<" ";
+          
+        }
+        int res = INT_MIN;
+        for(int i=0;i<n;i++){
+            res = max(res,(left[i]-right[i]-1)*heights[i]);
+        }
+        return res;
+    }
+};
