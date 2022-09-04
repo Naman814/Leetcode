c3c3bb609a28a0db1738ef9ff98d8f10b63ceb1f
Naman814 <73079318+Naman814@users.noreply.github.com>
2022-09-04 12:15:05 +0530

    Time: 26 ms (8.43%), Space: 17.5 MB (21.50%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,31 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         if(!root) return 0;
         queue<pair<TreeNode*,int>> q;
         q.push({root,0});
-        int res = 0;
+        int res=0;
         while(!q.empty()){
-            int n = q.size();
-            int currmin = q.front().second,leftidx,rightidx;
-            for(int i=0;i<n;i++){
-                long long int currid = q.front().second;
-                TreeNode * temp = q.front().first;
+            int currMin = q.front().second;
+            int sz = q.size();
+            int lidx,ridx;
+            for(int i=0;i<sz;i++){
+                auto currNode = q.front().first;
+                long long int curr = q.front().second - currMin;
                 q.pop();
-                if(i==0){
-                    leftidx = currid;
-                }
-                if(i==n-1){
-                    rightidx = currid;
-                }
-                if(temp->left){
-                    q.push({temp->left,currid*2+1});
-                }
-                if(temp->right){
-                    q.push({temp->right,currid*2+2});
-                }
+                if(i==0) lidx = curr;
+                if(i==sz-1) ridx = curr;
+                if(currNode->left) q.push({currNode->left,curr*2+1});
+                if(currNode->right) q.push({currNode->right,curr*2+2});
             }
-            res = max(res,rightidx-leftidx+1);
+            res = max(res,ridx-lidx+1);
+            
         }
         return res;
     }
