7bc82aadb24f869924d05405cd3926bb10ce4244
Naman814 <73079318+Naman814@users.noreply.github.com>
2024-07-11 00:57:56 +0530

    Time: 10 ms (7.41%), Space: 13.9 MB (61.11%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int cnt = 0;
-        for(int i=0;i<logs.size();++i){
-            string temp = "";
-            for(int j = logs[i].length()-1;j>=0;j--){
-                if(logs[i][j]=='/'  || logs[i][j]=='.'){
-                temp = logs[i][j] + temp;
-                }
-                else{
-                    break;
-                }
+         for(auto x:logs){
+         
+            if(x=="../"){
+                cnt = max(0,cnt-1);
             }
-            if(temp=="../" && cnt){
-                cnt--;
-            }
-            else if(temp=="/"){
+            else if(x!="./"){
                 cnt++;
             }
         }
